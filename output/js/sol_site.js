e26fa753f536ccc5f8e30603e95a609e6a41ddb9
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-02-28 14:22:13 +0100

    Publishing on 2017-02-28 13:22:13 GMT

---
+++ output/js/sol_site.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+    $(function () {
+        // Place any javascript you want to run in here!
+
+        $("#mobile-nav i").click(function () {
+            $("#mobile-nav #menu").toggle()
+        })
+    })
+
+})(jQuery);
