54e966e3c4a120b4d5d20afe07d0c6064861366c
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-03-11 13:41:49 +0100

    Publishing on 2017-03-11 12:41:49 GMT

--- theme/js/init.js
+++ theme/js/init.js
@@ -7,12 +7,13 @@
 skel.init({
 	reset: 'full',
 	breakpoints: {
-		global:		{ range: '*', href: '/css/style.css', containers: 1400, grid: { gutters: 50 } },
-		wide:		{ range: '-1680', href: '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-		normal:		{ range: '-1280', href: '/css/style-normal.css', containers: 960, lockViewport: true },
-		narrow:		{ range: '-980', href: '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
-		narrower:	{ range: '-840', href: '/css/style-narrower.css', grid: { collapse: 1 } },
-		mobile:		{ range: '-640', href: '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
+		// Default stylesheet already loaded, no need to do it again.
+		// global:		{ range: '*', href: '/theme/css/style.min.css', containers: 1400, grid: { gutters: 50 } },
+		wide:		{ range: '-1680', href: '/theme/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+		normal:		{ range: '-1280', href: '/theme/css/style-normal.css', containers: 960, lockViewport: true },
+		narrow:		{ range: '-980', href: '/theme/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+		narrower:	{ range: '-840', href: '/theme/css/style-narrower.css', grid: { collapse: 1 } },
+		mobile:		{ range: '-640', href: '/theme/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
 	}
 }, {
 	layers: {
